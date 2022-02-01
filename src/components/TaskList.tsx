db42b4212fc2089f2c10b9069c5648437b863276
Beatriz da Silva Carletti <Bia_carletti@hotmail.com>
2022-02-01 11:02:15 -0300

    finalizando

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -29,7 +29,10 @@ export function TaskList() {
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-    const checkTasks = tasks.splice(id )
+    const checkTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task)
 
     setTasks(checkTasks)
   }
